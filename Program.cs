00029f28d4b282c50e8a49c9af86ce95c84b6c66
Leo Cookson <10001481@BCS.NET.NZ>
2017-08-28 11:28:36 +1200

    First

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Problem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        Console.Clear();
+        var number = 0;
+        var count = 9;
+      Console.WriteLine("This Program will ask for ten numbers then tell you the total and average");
+        for(var i = 0; i <= count; i++)
+      { 
+     Console.WriteLine("Please enter a number");
+     
+     var Input = int.Parse(Console.ReadLine());
+
+     number += Input;
+     
+      }
+      Console.WriteLine($"The Total Number is {number}");
+      Console.WriteLine($"The Average is {number / 9}");
+     
+
+
+
+
+        
+			
+			Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine("Press <Enter> to quit the program");
+            Console.ReadKey();
+        }
+    }
+}
